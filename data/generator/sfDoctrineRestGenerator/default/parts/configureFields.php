e3247bd4e76ea6ed67fe8d69e9be8b216ee7591f
Dirk Nederveen <dirk@recras.nl>
2014-01-10 16:24:16 +0100

    Apply embedded_relations_fields in configureFields

--- data/generator/sfDoctrineRestGenerator/default/parts/configureFields.php
+++ data/generator/sfDoctrineRestGenerator/default/parts/configureFields.php
@@ -8,6 +8,7 @@
   protected function configureFields()
   {
 <?php
+$embedded_relations_fields = $this->configuration->getValue('default.embedded_relations_fields');
 $fields = $this->configuration->getValue('default.fields');
 $specific_configuration_directives = false;
 
@@ -19,10 +20,44 @@ foreach ($fields as $field => $configuration)
     continue;
   }
 }
+foreach ($embedded_relations_fields as $embed => $e_r_fields)
+{
+  foreach ($e_r_fields as $e_r_fields => $configuration)
+  {
+    if (isset($configuration['date_format']) || isset($configuration['tag_name']) || isset($configuration['type']))
+    {
+      $specific_configuration_directives = true;
+      break 2;
+    }
+  }
+}
 ?>
 <?php if ($specific_configuration_directives): ?>
     foreach ($this->objects as $i => $object)
     {
+<?php foreach ($embedded_relations_fields as $embed => $e_r_fields): ?>
+      if (isset($object['<?php echo $embed; ?>']))
+      {
+<?php foreach ($e_r_fields as $field => $configuration): ?>
+<?php if (isset($configuration['date_format']) || isset($configuration['tag_name']) || isset($configuration['type'])): ?>
+        if (isset($object['<?php echo $embed; ?>']['<?php echo $field; ?>']))
+        {
+<?php if (isset($configuration['date_format'])): ?>
+          $object['<?php echo $embed; ?>']['<?php echo $field ?>'] = date('<?php echo $configuration['date_format'] ?>', strtotime($object['<?php echo $embed; ?>']['<?php echo $field ?>']));
+<?php endif; ?>
+<?php if (isset($configuration['type'])): ?>
+	     $object['<?php echo $embed; ?>']['<?php echo $field ?>'] = is_null($object['<?php echo $embed; ?>']['<?php echo $field ?>']) ? null : (<?php echo $configuration['type'] ?>) $object['<?php echo $embed; ?>']['<?php echo $field ?>'];
+<?php endif; ?>
+<?php if (isset($configuration['tag_name'])): ?>
+          $object['<?php echo $embed; ?>']['<?php echo $configuration['tag_name'] ?>'] = $object['<?php echo $embed; ?>']['<?php echo $field ?>'];
+          unset($object['<?php echo $embed; ?>']['<?php echo $field ?>']);
+<?php endif; ?>
+        }
+<?php endif; ?>
+<?php endforeach; ?>
+      }
+
+<?php endforeach; ?>
 <?php foreach ($fields as $field => $configuration): ?>
 <?php if (isset($configuration['date_format']) || isset($configuration['tag_name']) || isset($configuration['type'])): ?>
       if (isset($object['<?php echo $field ?>']))
