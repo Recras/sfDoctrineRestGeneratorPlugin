e68c964f89a09662498f22a908a5b89b2d28bf40
Dirk Nederveen <dirk@recras.nl>
2014-01-31 10:07:33 +0100

    factor validateCreate to sfDoctrineRestGeneratorActions

--- lib/actions/sfDoctrineRestGeneratorActions.class.php
+++ lib/actions/sfDoctrineRestGeneratorActions.class.php
@@ -51,4 +51,20 @@ class sfDoctrineRestGeneratorActions extends sfActions
       }
     }
   }
+
+  /**
+   * Applies the creation validators to the payload posted to the service
+   *
+   * @param   string   $payload  A payload string
+   */
+  public function validateCreate($payload)
+  {
+    $params = $this->parsePayload($payload);
+
+    $validators = $this->getCreateValidators();
+    $this->validate($params, $validators);
+
+    $postvalidators = $this->getCreatePostValidators();
+    $this->postValidate($params, $postvalidators);
+  }
 }
