00c8232aedc0fd5d9ec0998c02ff77707434beb1
Dirk Nederveen <dirk@recras.nl>
2014-02-04 14:15:57 +0100

    Add compare operators to query generator

--- data/generator/sfDoctrineRestGenerator/default/parts/query.php
+++ data/generator/sfDoctrineRestGenerator/default/parts/query.php
@@ -130,6 +130,22 @@ $pagination_page_size = $this->configuration->getValue('get.pagination_page_size
       unset($params['<?php echo $name ?>']);
     }
 <?php endif; ?>
+<?php if (isset($filters[$name]['compare'])): ?>
+<?php $operators = array(
+  'less' => '<',
+  'lessEqual' => '<=',
+  'greater' => '>',
+  'greaterEqual' => '>=',
+); ?>
+<?php foreach ($filters[$name]['compare'] as $operation => $opAlias): ?>
+<?php $opAlias = $opAlias == null ? $operation : $opAlias; ?>
+    if (isset($params['<?php echo $name . ucfirst($opAlias) ?>']))
+    {
+      $q->andWhere($this->model.'.<?php echo $name ?> <?php echo $operators[$operation] ?> ?', $params['<?php echo $name . ucfirst($opAlias) ?>']);
+      unset($params['<?php echo $name . ucfirst($opAlias) ?>']);
+    }
+<?php endforeach; ?>
+<?php endif; ?>
 <?php endforeach; ?>
     foreach ($params as $name => $value)
     {
