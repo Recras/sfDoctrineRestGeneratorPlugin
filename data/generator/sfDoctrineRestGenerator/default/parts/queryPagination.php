672c24db4b25e409448eef332aed806bf47095dc
Dirk Nederveen <dirk@recras.nl>
2014-10-20 12:16:40 +0200

    simplify flow of paginations

--- data/generator/sfDoctrineRestGenerator/default/parts/queryPagination.php
+++ data/generator/sfDoctrineRestGenerator/default/parts/queryPagination.php
@@ -9,21 +9,11 @@
   {
 <?php
 $max_items = $this->configuration->getValue('get.max_items');
-if ($max_items > 0):
-?>
-    $limit = <?php echo $max_items; ?>;
-
-<?php endif; ?>
-<?php
 $pagination_custom_page_size = $this->configuration->getValue('get.pagination_custom_page_size');
 $pagination_enabled = $this->configuration->getValue('get.pagination_enabled');
-$pagination_page_size = $this->configuration->getValue('get.pagination_page_size'); ?>
+$pagination_page_size = $this->configuration->getValue('get.pagination_page_size');
+?>
 <?php if ($pagination_enabled): ?>
-    if (!isset($params['page']))
-    {
-      $params['page'] = 1;
-    }
-
     $page_size = <?php echo $pagination_page_size; ?>;
 <?php if ($pagination_custom_page_size): ?>
 
@@ -35,16 +25,19 @@ $pagination_page_size = $this->configuration->getValue('get.pagination_page_size
 
 <?php endif; ?>
 <?php if ($max_items > 0): ?>
-    $limit = min($page_size, $limit);
-    $page_size = $limit;
-<?php else: ?>
-    $limit = $page_size;
+    $page_size = $page_size === 0 ? <?php echo $max_items; ?> : $page_size;
 <?php endif; ?>
+
+    if (!isset($params['page']))
+    {
+      $params['page'] = 1;
+    }
     $query->offset(($params['page'] - 1) * $page_size);
     unset($params['page']);
-<?php endif; ?>
-<?php if ($max_items > 0 || $pagination_enabled): ?>
-    $query->limit($limit);
+
+    $query->limit($page_size);
+<?php elseif ($max_items > 0): // !$pagination_enabled ?>
+    $query->limit(<?php echo $max_items; ?>);
 <?php endif; ?>
 
     return $query;
