58414e33921e12169eb901be83641d4b5c84bd36
Dirk Nederveen <dirk@recras.nl>
2015-09-02 15:36:54 +0200

    Disambiguate table of sort column

--- data/generator/sfDoctrineRestGenerator/default/parts/querySort.php
+++ data/generator/sfDoctrineRestGenerator/default/parts/querySort.php
@@ -1,6 +1,12 @@
 <?php $sort_custom = $this->configuration->getValue('get.sort_custom'); ?>
 <?php $sort_default = $this->configuration->getValue('get.sort_default'); ?>
 <?php if ($sort_default || $sort_custom): ?>
+<?php
+$columns = array();
+foreach ($this->getColumns() as $column) {
+	$columns[] = $column->getFieldName();
+}
+?>
   /**
    * Add sort clauses from "sort_default" and "sort_custom" fields
    *
@@ -9,26 +15,32 @@
    */
   function querySort(Doctrine_Query_Abstract $query, array &$params)
   {
+    $columns = <?php echo var_export($columns, true); ?>;
 <?php if ($sort_default && count($sort_default) == 2): ?>
-    $sort = '<?php echo $sort_default[0] ?> <?php echo $sort_default[1] ?>';
+    $sort = array('<?php echo $sort_default[0] ?>', '<?php echo $sort_default[1] ?>');
+<?php else: ?>
+    $sort = array();
 <?php endif; ?>
 <?php if ($sort_custom): ?>
     if (isset($params['sort_by']))
     {
-      $sort = $params['sort_by'];
+      $sort[0] = $params['sort_by'];
       unset($params['sort_by']);
 
       if (isset($params['sort_order']))
       {
-        $sort .= ' '.$params['sort_order'];
+        $sort[1] = $params['sort_order'];
         unset($params['sort_order']);
       }
     }
 <?php endif; ?>
 
-    if (isset($sort))
+    if ($sort)
     {
-      $query->orderBy($sort);
+      if (in_array($sort[0], $columns)) {
+        $sort[0] = $this->model . '.' . $sort[0];
+      }
+      $query->orderBy($sort[0] . ' ' . $sort[1]);
     }
   }
 <?php else: ?>
